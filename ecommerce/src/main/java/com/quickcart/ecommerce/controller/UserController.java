f94c4455a63e597ab7c15565040c9df74ed6327e
AmruthaHavale <amruthakhavale@gmail.com>
2026-06-20 11:58:54 +0530

    customisation of home page

--- ecommerce/src/main/java/com/quickcart/ecommerce/controller/UserController.java
+++ ecommerce/src/main/java/com/quickcart/ecommerce/controller/UserController.java
@@ -54,7 +54,7 @@ public class UserController
         if (result.hasErrors()) {
             return "login";
         }
-        return "redirect:/home";
+        return "redirect:/products/list";
     }
 
 
